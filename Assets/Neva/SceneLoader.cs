a24157e087cc33639b5ffc4c36d53b7fab427066
Neva <enevaozturk@gmail.com>
2023-07-15 21:03:30 +0300

    SceneDelay

--- Assets/Neva/SceneLoader.cs
+++ Assets/Neva/SceneLoader.cs
@@ -5,7 +5,14 @@ using UnityEngine.SceneManagement;
 
 public class SceneLoader : MonoBehaviour
 {
+    string sceneName;
+
     public void LoadScene(string sceneName)
+    {
+        this.sceneName = sceneName;
+        Invoke("DelayedLoad", 0.125f);
+    }
+    public void DelayedLoad ()
     {
         SceneManager.LoadScene(sceneName);
     }
