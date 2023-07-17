31d58e35d9c2f8659bddf3dc0527bd4c5ea93f34
Elif Gümüştaş <elif_gumus_tas@hotmail.com>
2023-07-17 21:17:15 +0300

    Press E to interract

--- Assets/Elif/Scripts_E/deneme.cs
+++ Assets/Elif/Scripts_E/deneme.cs
@@ -1,16 +1,18 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class deneme : MonoBehaviour
 {
     public WinKey wk;
     
-    private void OnTriggerEnter(Collider other) {
+    private void OnTriggerStay(Collider other) {
         
-         if(other.gameObject.tag == "Player")
-         {
+         if(other.gameObject.tag == "Player" && Input.GetKeyDown("E"))
+         { 
 
+            //SceneManager.LoadScene(8); 
             wk.GainKey();
 
             Debug.Log("Denemeanahtarı");
